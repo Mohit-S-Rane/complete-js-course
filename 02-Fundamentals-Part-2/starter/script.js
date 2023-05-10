a69c07c409b37afe264fd3be44889731650069a2
MSRane <mohitsrane16@gmail.com>
2023-05-10 22:54:22 +0530

    Object method is done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -163,78 +163,144 @@
 // const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 // console.log(ages);
 
+// ///////////////////////////////////////
+// // Basic Array Operations (Methods)
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// // Add elements
+// const newLength = friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('John');
+// console.log(friends);
+
+// // Remove elements
+// friends.pop(); // Last
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift(); // First
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+// friends.unshift('Bob')
+// console.log(friends.indexOf('Bob'));
+
+// friends.push(23);
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+// console.log(friends.includes(23));
+
+// if (friends.includes('Steven')) {
+//   console.log('You have a friend called Steven');
+// }
+
+// ///////////////////////////////////////
+// // Coding Challenge #2
+
+// const calcTip = (bill) => bill >= 50 && bill <= 100 ? bill * 0.15 : bill * 0.2
+
+// const bills = [125, 555, 44]
+
+// const trip = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+
+// const totals = [bills[0] + trip[0] + bills[1] + trip[1] + bills[2] + trip[2]]
+
+// console.log(bills, trip, totals);
+
 ///////////////////////////////////////
-// Basic Array Operations (Methods)
-const friends = ['Michael', 'Steven', 'Peter'];
+// Introduction to Objects
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
 
-// Add elements
-const newLength = friends.push('Jay');
-console.log(friends);
-console.log(newLength);
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven']
+// };
 
-friends.unshift('John');
-console.log(friends);
+// ///////////////////////////////////////
+// // Dot vs. Bracket Notation
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven']
+// };
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
 
-// Remove elements
-friends.pop(); // Last
-const popped = friends.pop();
-console.log(popped);
-console.log(friends);
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
 
-friends.shift(); // First
-console.log(friends);
+// // console.log(jonas.'last' + nameKey)
 
-console.log(friends.indexOf('Steven'));
-friends.unshift('Bob')
-console.log(friends.indexOf('Bob'));
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
 
-friends.push(23);
-console.log(friends.includes('Steven'));
-console.log(friends.includes('Bob'));
-console.log(friends.includes(23));
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+// }
 
-if (friends.includes('Steven')) {
-  console.log('You have a friend called Steven');
-}
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+
+// // Challenge
+// // "Jonas has 3 friends, and his best friend is called Michael"
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best firend is called ${jonas.friends[0]}`);
 
 ///////////////////////////////////////
-// Coding Challenge #2
+// Object Methods
 
-const calcTip = (bill) => bill >= 50 && bill <= 100 ? bill * 0.15 : bill * 0.2
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
 
-const bills = [125, 555, 44]
+  // calcAge: function (birthYeah) {
+  //   return 2037 - birthYeah;
+  // }
 
-const trip = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYeah;
+  // }
 
-const totals = [bills[0] + trip[0] + bills[1] + trip[1] + bills[2] + trip[2]]
+  calcAge: function () {
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
 
-console.log(bills, trip, totals);
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+  }
+};
 
+console.log(jonas.calcAge());
+console.log(jonas);
 
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(`${jonas.firstName} is a ${jonas.age}-year old teacher, and he has ${jonas.hasDriversLicense ? 'a' : 'no'} driver's license.`);
 
 
 
